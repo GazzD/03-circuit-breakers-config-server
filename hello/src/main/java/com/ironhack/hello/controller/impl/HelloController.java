3ef762e83b83e352373c181f7cc3fec604a8f043
Víctor Cardozo <vcardozof@gmail.com>
2021-02-17 14:30:47 +0100

    Adds spring cloud config

--- hello/src/main/java/com/ironhack/hello/controller/impl/HelloController.java
+++ hello/src/main/java/com/ironhack/hello/controller/impl/HelloController.java
@@ -1,12 +1,32 @@
 package com.ironhack.hello.controller.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+    @Value("${important.value}")
+    private String value1;
+
+    @Value("${important.value2}")
+    private String value2;
+
+    @Value("${important.value3}")
+    private String value3;
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @GetMapping("/hello")
     public String hello() {
+
+        logger.info(value1);
+        logger.info(value2);
+        logger.info(value3);
+
         return "HELL-O";
     }
 }
